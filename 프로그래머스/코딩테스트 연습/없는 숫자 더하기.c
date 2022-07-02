0e6f2b11b93fdf72050367776f92f7fbe43f17bc
ⒶⓁⒾⒸⒾⒶ JAYUN <83991079+chojangg@users.noreply.github.com>
2022-07-02 12:15:14 +0900

    다른 풀이

--- 프로그래머스/코딩테스트 연습/없는 숫자 더하기.c
+++ 프로그래머스/코딩테스트 연습/없는 숫자 더하기.c
@@ -5,20 +5,9 @@
 // numbers_len은 배열 numbers의 길이입니다.
 int solution(int numbers[], size_t numbers_len) {
     int answer = 0;
-    int num[9];
-    for (int i = 0; i < 9; i++) {
-        num[i] = i + 1;
-    }
-    for (int i = 0; i < numbers_len; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (num[j] == numbers[i]) {
-                num[j] = 0;
-            }
-        }
-    }
-    for (int i = 0; i < 9; i++) {
-        answer += num[i];
-    }
 
-    return answer;
+    for (int i = 0; i < numbers_len; i++)
+        answer += numbers[i];
+
+    return 45 - answer;
 }
